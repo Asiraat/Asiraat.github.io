579514989e2d1b991ca3573adf54c3343dae9d48
あしらーと <154391891+Asiraat@users.noreply.github.com>
2024-07-28 11:18:37 +0900

    Update blog.js

--- script/blog.js
+++ script/blog.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const blogListElement = document.getElementById('blog-list');
     const blogPosts = [
-       'https://Asiraat.github.io/blogs/example1.md',
        'https://raw.githubusercontent.com/Asiraat/Asiraat.github.io/main/blogs/example1.md'
         // ブログ記事へのパスをここに追加
     ];
